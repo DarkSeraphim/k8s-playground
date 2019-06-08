a6dcb2ddf2d27c4bed815e9015e8af89ee27c815
Mark Hendriks <mark.hendriks@castawaylabs.com>
2019-06-08 01:48:59 +0100

    Create deployment and service configuration, and echo back the hostname for load balancing verification

--- project-1/index.js
+++ project-1/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('From: ' + process.env.HOSTNAME + '\nMessage: ');
   req.pipe(res);
 });
 
